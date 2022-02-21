d8a005edc05916fa8d4ca3770aaa677ba1b8f305
andreeaaraduc <raducandreea19@stud.ase.ro>
2022-02-21 11:16:20 +0200

    lab1 commit

--- src/lab1.java
+++ src/lab1.java
@@ -1,2 +1,17 @@
-public class lab1 {
+public abstract class Animal {
+    private String nume;
+
+    public abstract void  mananca(String tipMancare);
+
+    public static class Girafa extends Animal {
+
+        public Girafa(String nume) {
+            super(nume);
+        }
+
+        @Override
+        public void mananca(String tipMancare) {
+            System.out.println("Animal.Girafa " + getNume() + "mananca " + tipMancare);
+        }
+    }
 }

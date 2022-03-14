3aa139f851358cf8aa344296debf80e4d787f85c
andreeaaraduc <raducandreea19@stud.ase.ro>
2022-03-14 11:11:28 +0200

    lab3 commit

--- CTS_lab2/src/Clase_LAB2/Program.java
+++ CTS_lab2/src/Clase_LAB2/Program.java
@@ -1,16 +1,25 @@
 package clase;
 
+import clase.readers.AngajatiReader;
+import clase.readers.IReader;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			IReader reader = new AngajatiReader("angajati.txt");
+			listaAngajati = reader.readAplicanti();
+			Aplicant.setPragPunctaj(90);
+			for (Aplicant angajat : listaAngajati) {
 				System.out.println(angajat.toString());
+				angajat.afisareRezultat();
+				angajat.afisareSumaBani(2000);
+			}
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
